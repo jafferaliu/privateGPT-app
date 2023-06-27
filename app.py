a24415e84b451c65f142127d10e009573d6307a0
jafferaliu <80578901+jafferaliu@users.noreply.github.com>
2023-06-27 20:42:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,97 +1,124 @@
 from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import Chroma
+from langchain.llms import GPT4All, LlamaCpp
 import os
-import streamlit as st
-import requests
-from typing import List
-import json
-import socket
-from urllib3.connection import HTTPConnection
-import time
+from fastapi import FastAPI, UploadFile, File
+from typing import List, Optional
+import urllib.parse
 
-API_BASE_URL = os.environ.get("API_BASE_URL")
+app = FastAPI()
+
 
 load_dotenv()
 
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 
+model_type = os.environ.get('MODEL_TYPE')
+model_path = os.environ.get('MODEL_PATH')
+model_n_ctx = os.environ.get('MODEL_N_CTX')
+source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
+
 from constants import CHROMA_SETTINGS
-import chromadb
 
-def list_of_collections():
-    client = chromadb.Client(CHROMA_SETTINGS)
-    return (client.list_collections())
+async def test_embedding():
+    # Create the folder if it doesn't exist
+    os.makedirs(source_directory, exist_ok=True)
+    # Create a sample.txt file in the source_documents directory
+    file_path = os.path.join("source_documents", "test.txt")
+    with open(file_path, "w") as file:
+        file.write("This is a test.")
+    # Run the ingest.py command
+    os.system('python ingest.py --collection test')
+    # Delete the sample.txt file
+    os.remove(file_path)
+    print("embeddings working")
+
+async def model_download():
+    match model_type:
+        case "LlamaCpp":
+            url = "https://gpt4all.io/models/ggml-gpt4all-l13b-snoozy.bin"
+        case "GPT4All":
+            url = "https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin"
+        case "vicuna":
+            url = "https://gpt4all.io/models/ggml-vicuna-7b-1.1-q4_2.bin"
+    folder = "models"
+    parsed_url = urllib.parse.urlparse(url)
+    filename = os.path.join(folder, os.path.basename(parsed_url.path))
+    # Check if the file already exists
+    if os.path.exists(filename):
+        print("File already exists.")
+        return
+    # Create the folder if it doesn't exist
+    os.makedirs(folder, exist_ok=True)
+    # Run wget command to download the file
+    os.system(f"wget {url} -O {filename}")
+    global model_path 
+    model_path = filename
+    os.environ['MODEL_PATH'] = filename
+    print("model downloaded")
     
-def main():
-    st.title("AI For Document Embedding and Retrieval")
-    
-    # Document upload section
-    st.header("Document Upload")
-    files = st.file_uploader("Upload document", accept_multiple_files=True)
-    # collection_name = st.text_input("Collection Name") not working for some reason
-    if st.button("Embed"):
-        embed_documents(files, "collection_name")
-    
-    # Query section
-    st.header("Document Retrieval")
-    collection_names = get_collection_names()
-    selected_collection = st.selectbox("Select a document", collection_names)
-    if selected_collection:
-        query = st.text_input("Query")
-        if st.button("Retrieve"):
-            retrieve_documents(query, selected_collection)
 
-def embed_documents(files:List[st.runtime.uploaded_file_manager.UploadedFile], collection_name:str):
-    endpoint = f"{API_BASE_URL}/embed"
-    files_data = [("files", file) for file in files]
-    data = {"collection_name": collection_name}
-
-    response = requests.post(endpoint, files=files_data, data=data)
-    if response.status_code == 200:
-        st.success("Documents embedded successfully!")
-    else:
-        st.error("Document embedding failed.")
-        st.write(response.text)
+# Starting the app with embedding and llm download
+@app.on_event("startup")
+async def startup_event():
+    await test_embedding()
+    await model_download()
 
 
-def get_collection_names():
+# Example route
+@app.get("/")
+async def root():
+    return {"message": "Hello, the APIs are now ready for your embeds and queries!"}
 
-    collections = list_of_collections()
-    return [collection.name for collection in collections]
+@app.post("/embed")
+async def embed(files: List[UploadFile], collection_name: Optional[str] = None):
 
-
-
-def retrieve_documents(query: str, collection_name: str):
-    start_time = time.time()
-    endpoint = f"{API_BASE_URL}/retrieve"
-    data = {"query": query, "collection_name": collection_name}
-
-    # Modify socket options for the HTTPConnection class
-    HTTPConnection.default_socket_options = (
-        HTTPConnection.default_socket_options + [
-            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
-            (socket.SOL_TCP, socket.TCP_KEEPIDLE, 45),
-            (socket.SOL_TCP, socket.TCP_KEEPINTVL, 10),
-            (socket.SOL_TCP, socket.TCP_KEEPCNT, 6)
-        ]
-    )
-    
-    response = requests.post(endpoint, params=data)
-    inference_time = time.time() - start_time
-    print(f'Total Inference Time: {inference_time} seconds')
-    
-    if response.status_code == 200:
-        result = response.json()
-        st.subheader("Results")
-        st.text(result["results"])
+    saved_files = []
+    # Save the files to the specified folder
+    for file in files:
+        file_path = os.path.join(source_directory, file.filename)
+        saved_files.append(file_path)
         
-        #st.subheader("Documents")
-        #for doc in result["docs"]:
-        #    st.text(doc)
-    else:
-        st.error("Failed to retrieve documents.")
-        st.write(response.text)
+        with open(file_path, "wb") as f:
+            f.write(await file.read())
+        
+        if collection_name is None:
+            # Handle the case when the collection_name is not defined
+            collection_name = file.filename
+    
+    os.system(f'python ingest.py --collection {collection_name}')
+    
+    # Delete the contents of the folder
+    [os.remove(os.path.join(source_directory, file.filename)) or os.path.join(source_directory, file.filename) for file in files]
+    
+    return {"message": "Files embedded successfully", "saved_files": saved_files}
+
+@app.post("/retrieve")
+async def query(query: str, collection_name:str):
+    
+    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    db = Chroma(persist_directory=persist_directory,collection_name=collection_name, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+    retriever = db.as_retriever()
+    # Prepare the LLM
+    callbacks = [StreamingStdOutCallbackHandler()]
+    match model_type:
+        case "LlamaCpp":
+            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
+        case "GPT4All":
+            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)  
+        case _default:
+            print(f"Model {model_type} not supported!")
+            exit;
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+    
+    # Get the answer from the chain
+    res = qa(query)
+    print(res)   
+    answer, docs = res['result'], res['source_documents']
 
 
-if __name__ == "__main__":
-    main()
+    return {"results": answer, "docs":docs}
