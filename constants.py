edac16115c47f8967a568ddcc595294585b6d50f
jafferaliu <80578901+jafferaliu@users.noreply.github.com>
2023-07-08 14:55:07 +0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,6 +4,12 @@ from chromadb.config import Settings
 
 load_dotenv()
 
+
+greeting = ["hi", "hello", "hello!", "hey" , "hi buddy", "good morning", "good afternoon", "good evening"]
+name_check = ["what is your name", "can you tell your name", "your name please", "can you please tell your name"]
+change_name = ["can you change your name", "please change your name to", "can you please change your name", 
+               "can you update your name to", "change your name to" ,"this is your name"]
+
 # Define the folder for storing database
 PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY')
 
